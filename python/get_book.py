dc947e62b5f38f9ee0019cf7a8005dbc3c3a2fae
Xiaochuan Wang <xiaocwan@redhat.com>
2017-07-24 00:02:24 -0500

    Update and rename scrape_book.py to get_book.py

--- python/get_book.py
+++ python/get_book.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import requests, os, time
+import requests, os, time, sys
 from bs4 import BeautifulSoup
 
-def get_context_from_page(id):
+def get_context_from_page(bookid,pageid):
 	text=""
-	url="http://www.kanunu8.com/book/4210/%d.html"%id
+	url="http://www.kanunu8.com/book/%d/%d.html"%(bookid,pageid)
 	web_data=requests.get(url)
 	web_data.encoding = "GBK"
 	soup=BeautifulSoup(web_data.text)
@@ -23,18 +23,23 @@ def get_context_from_page(id):
 
 if __name__ == '__main__':
 	## this book is scraped from http://www.kanunu8.com/files/writer/4208.html
-	bookname = "丁庄梦 - 阎连科"
+	bookname = "坚硬如水 - 阎连科"
+	start = bookid = 4213
+	start_page = 46355
+	end_page = 46369
+	
 	content = bookname
 	print os.getcwd()
 	f = open(bookname, 'w')
 	i = 1
-	for id in range(46372, 46391):
+	end = end_page + 1
+	for pageid in range(start_page, end):
 		try:
 			print 'Chapter' + str(i)
-			content = content + str(get_context_from_page(id))
+			content = content + str(get_context_from_page(bookid,pageid))
 			i = i+1
 		except Exception,e:
-			print "ignore page error: "+str(id)
+			print "ignore page error: "+str(pageid)
 			print e
 			continue
 	f.write(content)
